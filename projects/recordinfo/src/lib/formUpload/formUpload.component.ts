19cc5adffd3be44c2589adb263570985a3cf878f
zhangmingqian01 <1965748469@qq.com>
2020-06-02 09:09:26 +0800

    著录的电子文件上传

--- projects/recordinfo/src/lib/formUpload/formUpload.component.ts
+++ projects/recordinfo/src/lib/formUpload/formUpload.component.ts
@@ -7,67 +7,68 @@ import { NzNotificationService } from 'ng-zorro-antd';
     styleUrls: ['./formUpload.component.scss'],
 })
 export class FormUploadComponent implements OnInit {
-    public uploader: FileUploader;
-    @Input() disabledUpload : any = false 
+    // public uploader: FileUploader;
+    @Input() disabledUpload: any = false
     @Input() attrName: any;
-    @Input() uploadUrl : string;
-    @Input() additionalParams : any = {};
-    @Input() updataDetail:any
+    @Input() uploadUrl: string;
+    @Input() additionalParams: any = {};
+    @Input() updataDetail: any
 
-    @Input() ApiUrl : any
-    @Input() baseUrl : string 
-    @Input() AuthenticationService : any
+    @Input() ApiUrl: any
+    @Input() baseUrl: string
+    @Input() AuthenticationService: any
 
     @Output() uploadFinish: EventEmitter<any> = new EventEmitter();
+    uploader = new FileUploader({
+        autoUpload: true,
+        // url: this.baseUrl + this.ApiUrl[url],   
+        url: '/ermsapi/resource/upload_file',
+        headers: [
+            { name: 'accessToken', value: 'db4d40c5a4dfe5408bca993c66f1664d' }
+        ]
+        , additionalParameter: {
+        },
+    });
+
     constructor(
-        private notification: NzNotificationService,                    
+        private notification: NzNotificationService,
     ) {
-        
-    }
-
-    ngOnInit() {
-        let url 
-        this.uploadUrl == 'importRecord' ? url = 'importRecord' : url = 'upload'
-        if (!this.baseUrl || !this.ApiUrl) return 
-        this.uploader = new FileUploader({
-            autoUpload: true,
-            url: this.baseUrl + this.ApiUrl[url],   
-            // url : '/ermsapi/resource/upload_file',
-            headers : [
-                {name : 'accessToken',value : this.AuthenticationService.getAccessToken()}
-              ]
-            ,additionalParameter: {        
-            },
-        });
-
-        this.uploader.onBeforeUploadItem = (item) =>{   
-            for(let key in this.additionalParams) {
+        this.uploader.onBeforeUploadItem = (item) => {
+            for (let key in this.additionalParams) {
                 this.uploader.options.additionalParameter[key] = this.additionalParams[key]
             }
             this.uploader.options.additionalParameter['relativePath'] = '/' + this.attrName + '/' + item._file.name
-          }
-        this.uploader.onSuccessItem = (item, res) => {         
+        }
+        this.uploader.onSuccessItem = (item, res) => {
             if (res) {
-                let data = JSON.parse(res)           
-                this.uploadFinish.emit({ 
-                    data : data,                    
-                    attrName:this.attrName,
-                    name:item._file.name,
-                    size:item._file.size,                                       
-                })     
+                let data = JSON.parse(res)
+                this.uploadFinish.emit({
+                    data: data,
+                    attrName: this.attrName,
+                    name: item._file.name,
+                    size: item._file.size,
+                })
                 item.remove()
-                return                       
+                return
             }
             item.isSuccess = false
             item.isError = true
         }
-        this.uploader.onErrorItem = (item,res) =>{  
-            let data = JSON.parse(res)  
+        this.uploader.onErrorItem = (item, res) => {
+            let data = JSON.parse(res)
             this.notification.blank(
                 data.message,
                 ''
-              );                         
+            );
         }
+    }
+
+    ngOnInit() {
+        let url
+        this.uploadUrl == 'importRecord' ? url = 'importRecord' : url = 'upload'
+        if (!this.baseUrl || !this.ApiUrl) return
+
+
     }
 
 }
